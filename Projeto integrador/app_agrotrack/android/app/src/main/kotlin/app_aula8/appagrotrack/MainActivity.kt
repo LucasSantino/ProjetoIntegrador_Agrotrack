a59899dacfbc080f243bdd1d4228d00bb13e0255
LucasSantino <lsantino541@gmail.com>
2025-05-18 16:14:22 -0300

    up

--- Projeto integrador/app_agrotrack/android/app/src/main/kotlin/app_aula8/appagrotrack/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package app_aula8.appagrotrack
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Projeto integrador/app_agrotrack/android/app/src/main/kotlin/app_aula8/appaula11/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package app_aula8.appaula11
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
