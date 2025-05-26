63efd049eb1ac3345f9d38c692be90f3d9dc9cec
LucasSantino <lsantino541@gmail.com>
2025-05-25 23:17:27 -0300

    Update SplashScreen.dart

--- app_agrotrack/lib/screen/SplashScreen.dart
+++ app_agrotrack/lib/screen/SplashScreen.dart
@@ -9,10 +9,21 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0.0;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
+
+    // Inicia a animação de fade-in após o primeiro frame renderizado
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+
+    // Navega para login após 5 segundos
+    Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const Login()),
@@ -22,15 +33,18 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
       body: Center(
-        child: Image(
-          image: AssetImage(
+        child: AnimatedOpacity(
+          opacity: _opacity,
+          duration: const Duration(seconds: 2),
+          curve: Curves.easeIn,
+          child: Image.asset(
             'assets/images/logoAgrotrack_sem fundo 2 - Copia.png',
+            width: 150,
+            height: 150,
           ),
-          width: 150,
-          height: 150,
         ),
       ),
     );
