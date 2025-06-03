d80107bb58d31f71b4fca6a9bfb9cd693c50cdfa
LucasSantino <lsantino541@gmail.com>
2025-06-02 22:17:29 -0300

    Update automacao.dart

--- app_agrotrack/lib/screen/automacao.dart
+++ app_agrotrack/lib/screen/automacao.dart
@@ -191,7 +191,7 @@ class _AutomacaoState extends State<Automacao> {
 
 
 
-/* código aterior antes da integração
+/* 
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
